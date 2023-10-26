6680566d9d8cae1bcb410e6001cc616854647395
Helekin <clsjs94@hotmail.com>
2023-10-25 21:10:44 -0500

    models & controllers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,13 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        var person = new Person()
+        {
+            Name = "Hellekin",
+            Age = 29
+        };
+
+        return View(person);
     }
 
     public IActionResult Privacy()
---
+++ Models/Person.cs
@@ -0,0 +1,8 @@
+namespace aspnet_01_easy_portfolio.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
